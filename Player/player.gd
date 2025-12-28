1de6288fb2be2d90fe1398cfe98a62b7d27455ba
nikita32 <nik3tos4@gmail.com>
2025-12-28 18:40:56 +0300

    gg

--- Player/player.gd
+++ Player/player.gd
@@ -20,40 +20,48 @@ var health = 100
 var gold = 0
 var state = MOVE
 var run_speed = 1
+var combo = false
+
+func _ready():
+	# Подключаем сигнал окончания анимации
+	animPlayer.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(delta: float) -> void:
+	# Гравитация
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Логика падения (только если мы движемся, чтобы не сбивать анимацию атаки)
+	if velocity.y > 0 and state == MOVE:
+		animPlayer.play("Fall")
+		
+	# Смерть
+	if health <= 0:
+		health = 0
+		set_physics_process(false) # Останавливаем физику при смерти
+		animPlayer.play("Death")
+		await animPlayer.animation_finished
+		queue_free()
+		get_tree().change_scene_to_file("res://menu.tscn")
+		return
+
 	match state:
 		MOVE:
 			move_state()
 		ATTACK:
-			pass
+			attack_state()
 		ATTACK2:
-			pass
+			attack2_state()
 		ATTACK3:
-			pass
+			attack3_state()
 		BLOCK:
 			block_state()
 		SLIDE:
 			slide_state()
 	
-	
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	if velocity.y > 0:
-		animPlayer.play("Fall")
-		
-	if health <= 0:
-		health = 0
-		animPlayer.play("Death")
-		await animPlayer.animation_finished
-		queue_free()
-		get_tree().change_scene_to_file("res://menu.tscn")
-
 	move_and_slide()
 
-func move_state ():
+func move_state():
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED * run_speed
@@ -66,11 +74,12 @@ func move_state ():
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		if velocity.y == 0:
 			animPlayer.play("Idle")
+			
 	if direction == -1:
 		anim.flip_h = true
-		
 	elif direction == 1:
 		anim.flip_h = false
+		
 	if Input.is_action_pressed("run"):
 		run_speed = 2
 	else:
@@ -81,14 +90,54 @@ func move_state ():
 			state = BLOCK
 		else:
 			state = SLIDE
-		
-func block_state ():
+			animPlayer.play("Slide") # Запускаем анимацию один раз при входе
+			
+	# ВОТ ЗДЕСЬ БЫЛА ОШИБКА
+	if Input.is_action_just_pressed("attack"):
+		start_attack(ATTACK, "Attack")
+
+# Вспомогательная функция для старта атаки
+func start_attack(new_state, anim_name):
+	state = new_state
+	velocity.x = 0
+	animPlayer.play(anim_name)
+
+func block_state():
 	velocity.x = 0
 	animPlayer.play("Block")
 	if Input.is_action_just_released("block"):
 		state = MOVE
 
 func slide_state():
-	animPlayer.play("Slide")
-	await animPlayer.animation_finished
-	state = MOVE
+	# Логика обрабатывается через сигнал animation_finished
+	pass
+
+func attack_state():
+	# Тут мы просто ждем ввода для комбо. Анимация уже играет.
+	if Input.is_action_just_pressed("attack") and combo == true:
+		start_attack(ATTACK2, "Attack2")
+
+func attack2_state():  
+	if Input.is_action_just_pressed("attack") and combo == true:
+		start_attack(ATTACK3, "Attack3")
+	
+func attack3_state():
+	pass # Ждем окончания анимации
+
+# Эта функция вызывается сама, когда любая анимация доигрывает до конца
+func _on_animation_finished(anim_name):
+	if anim_name == "Attack":
+		state = MOVE
+	elif anim_name == "Attack2":
+		state = MOVE
+	elif anim_name == "Attack3":
+		state = MOVE
+	elif anim_name == "Slide":
+		state = MOVE
+
+# Эту функцию нужно вызывать через AnimationPlayer (Add Track -> Call Method Track)
+# в тех кадрах анимации, где разрешено комбо
+func combo1():
+	combo = true
+	# Комбо открывается на короткое время, можно использовать таймер
+	# или выключить его в конце анимации
